ff5da4a759456dde55314a46ad4df63c9538c8a1
BoberGame <pankov.zhora@gmail.com>
2023-11-29 09:31:43 +1100

    Add bcrypto

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,47 @@
 import { useDispatch } from 'react-redux';
 import React from 'react';
 import styles from './Login.module.scss';
-import { Formik } from 'formik';
+import { Field, Formik } from 'formik';
 import Button from '../../components/UI/Button/Button';
 import { authActions } from '../../store/slices/authSlice';
+import { genSalt, hash } from 'bcryptjs';
+
+const FormField = ({ label, ...props }) => {
+  console.log(props.errors);
+  return (
+    <div className={styles.form__item}>
+      <label className={styles.form__label} htmlFor={props.id}>
+        {label}:
+      </label>
+      <Field className={styles.form__control} {...props} />
+      {/* {props.errors[props.name] && props.touched[props.name]} */}
+    </div>
+  );
+};
+
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  password: '',
+  group: '',
+};
 
 const Login = () => {
   const dispatch = useDispatch();
 
+  const hashPassword = async (password, newPassword) => {
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(password, salt);
+    const hashedNewPassword = await hash(newPassword, salt);
+    const passwordsMatch = hashedPassword === hashedNewPassword;
+  };
+
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} container`}>
       <div className={styles.window}>
         <h1 className={styles.title}>Вход в систему</h1>
         <Formik
-          initialValues={{ fullName: '', group: '' }}
+          initialValues={initialValues}
           onSubmit={(values, { setSubmitting }) => {
             setTimeout(() => {
               dispatch(authActions.signIn({ userData: values }));
@@ -24,31 +52,34 @@ const Login = () => {
           {({ values, handleSubmit, handleChange, onBlur, isSubmitting }) => (
             <form onSubmit={handleSubmit}>
               <div className={styles.form__item}>
-                <label className={styles.form__label} htmlFor="fullName">
-                  Фамилия и имя:
+                <label className={styles.form__label} htmlFor="firstName">
+                  Имя:
                 </label>
                 <input
                   className={styles.form__control}
                   type="text"
-                  id="fullName"
-                  name="fullName"
+                  id="firstName"
+                  name="firstName"
                   onChange={handleChange}
                   onBlur={onBlur}
                   value={values.fullName}
+                  autoComplete="firstName"
                 />
               </div>
+              <FormField type="text" name="lastName" id="lastName" label="Фамилия" />
               <div className={styles.form__item}>
-                <label className={styles.form__label} htmlFor="group">
-                  Группа:
+                <label className={styles.form__label} htmlFor="password">
+                  Пароль:
                 </label>
                 <input
                   className={styles.form__control}
-                  type="text"
-                  id="group"
-                  name="group"
+                  type="password"
+                  id="password"
+                  name="password"
                   onChange={handleChange}
                   onBlur={onBlur}
-                  value={values.group}
+                  value={values.password}
+                  autoComplete="password"
                 />
               </div>
               <div className={styles.footer}>
